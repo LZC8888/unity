a3c46a5ac7c92eccd009014b513e5b5d9cd62da4
刘浙川 <1729721249@qq.com>
2022-04-21 19:39:44 +0800

    完成角色移动跳跃

--- Assets/scripts/PlayerCtrl.cs
+++ Assets/scripts/PlayerCtrl.cs
@@ -7,19 +7,68 @@ public class PlayerCtrl : MonoBehaviour
     // Start is called before the first frame update
     public float MoveForce = 100.0f;
     public float MaxSpeed = 5;
-    private Rigidbody2D HeroBody;
+    public Rigidbody2D HeroBody;
+    [HideInInspector]
+    public bool bFaceRight = true;
+    [HideInInspector]
+    public bool bJump = false;
+    public float JumpForce = 500;
+    private Transform mGroundCheck;
     void Start()
     {
         HeroBody = GetComponent<Rigidbody2D>();
+        mGroundCheck = transform.Find("GroundCheck");
     }
 
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        if (Mathf.Abs(HeroBody.velocity.x)<MaxSpeed)
+        if (Mathf.Abs(HeroBody.velocity.x) < MaxSpeed)
         {
             HeroBody.AddForce(Vector2.right * h * MoveForce);
         }
+
+        if (Mathf.Abs(HeroBody.velocity.x) > 5)
+        {
+            HeroBody.velocity = new Vector2(Mathf.Sign(HeroBody.velocity.x) * MaxSpeed,
+                                            HeroBody.velocity.y);
+        }
+
+        if (h > 0 && !bFaceRight)
+        {
+            flip();
+        }
+        else if (h < 0 && bFaceRight)
+        {
+            flip();
+        }
+
+        if (Physics2D.Linecast(transform.position, mGroundCheck.position,
+                                1 << LayerMask.NameToLayer("Ground")))
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                bJump = true;
+            }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (bJump)
+        {
+            HeroBody.AddForce(Vector2.up * JumpForce);
+            bJump = false;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        bFaceRight = !bFaceRight;
     }
 }
